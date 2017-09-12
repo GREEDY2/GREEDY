f482c5f1899252c5130f4a8f38b2b47d4d4a6bab
MariusKurbakovas <marius.kurbakovas@gmail.com>
2017-09-12 17:03:16 +0300

    Added Unit Testing to project (UnitTest1 as an example)

---
+++ GREEDY.UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GREEDY.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            //var inside = something;
+
+            //act
+            //var outside = SomeClass.SomeMethod(SomeParams);
+
+            //assert
+            //Assert.AreEqual(inside, outside);
+        }
+    }
+}
