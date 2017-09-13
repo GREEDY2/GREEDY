9cb63517d89eb014c72718a911440d22cd3f9e74
MariusKurbakovas <marius.kurbakovas@gmail.com>
2017-09-13 18:50:12 +0300

    Added basic OCR (Tesseract). Works decent I think we can use this

---
+++ GREEDY/MainScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace GREEDY
+{
+    public partial class Greedy : Form
+    {
+        public Greedy()
+        {
+            InitializeComponent();
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOCR_Click(object sender, EventArgs e)
+        {
+            if (imageForOCR.ShowDialog() == DialogResult.OK)
+            {
+                var img = new Bitmap(imageForOCR.FileName);
+                var ocr = new TesseractEngine("../../../Data/tessdata", "eng", EngineMode.TesseractAndCube);
+                var page = ocr.Process(img);
+                textResult.Text = page.GetText();
+                GC.Collect();
+
+            }
+        }
+
+        private void textResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
