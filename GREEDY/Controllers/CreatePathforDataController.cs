13823ad5e907c039cb85d83fadd99d85365ef42f
Valentinas <valentinas.kaminskas@mif.stud.vu.lt>
2017-09-26 01:13:48 +0300

    Unit test for that class also has beed updated

--- GREEDY/Controllers/CreatePathforDataController.cs
+++ GREEDY/Controllers/CreatePathforDataController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace GREEDY.Controllers
 {
@@ -6,7 +7,18 @@ namespace GREEDY.Controllers
     {
         public void CreateAFolder(string filePath)
         {
+            if (Directory.Exists(filePath))
+            {
+                return;
+            }
+            else if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException("filePath");
+            }
+            else
+            {
             Directory.CreateDirectory(filePath);
+            }
         }
     }
 }
