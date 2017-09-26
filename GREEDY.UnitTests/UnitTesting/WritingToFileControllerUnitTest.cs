e53a04f547ae45edc25489f95d59f3915512a50d
valentk777 <31697821+valentk777@users.noreply.github.com>
2017-09-26 14:06:19 +0300

    Valentinas unist test (#16)
    * UnitTestCreated
    
    * IntegrationTest Added
    
    * Final Example of unit test and integration test. Need to create unit test for everyone in our persinal code
    
    * CreatePathForDataControler class has been updated
    
    * Unit test for that class also has beed updated
    
    * Update WritingToFileControllerIntegrationTest.cs

---
+++ GREEDY.UnitTests/UnitTesting/WritingToFileControllerUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using GREEDY.Models;
+using GREEDY.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Ploeh.AutoFixture;
+
+namespace GREEDY.UnitTests
+{
+    [TestClass]
+    public class WritingToFileControllerUnitTest
+    {
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("        ")]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void WriteToFile_IncorectFileName_ArgumentExcention (string filePath)
+        {
+            //arrange
+            var fixture = new Fixture();
+            Receipt receipt = fixture.Create<Receipt>();
+            
+            //act
+            new WritingToFileController().WriteToFile(filePath, receipt);
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [ExpectedException(typeof(ArgumentNullException))]
+
+        public void WriteToFile_NullFileName_ArgumentNullExcention(string filePath)
+        {
+            //arrange
+            var fixture = new Fixture();
+            Receipt receipt = fixture.Create<Receipt>();
+
+            //act
+            new WritingToFileController().WriteToFile(filePath, receipt);
+        }
+    }
+}
