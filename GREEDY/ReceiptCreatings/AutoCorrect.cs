39122b6d18fe5968bbf1b4c0e7d8ea21f466d4f4
Valentinas <valentinas.kaminskas@mif.stud.vu.lt>
2017-11-17 15:22:45 +0200

    AutoCorrect implementation has been finished but now not used because image formating issue

--- GREEDY/ReceiptCreatings/AutoCorrect.cs
+++ GREEDY/ReceiptCreatings/AutoCorrect.cs
@@ -1,57 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using GREEDY.Models;
-//using NHunspell;
-
-//namespace GREEDY.ReceiptCreatings
-//{
-//    class AutoCorrect
-//    {
-//        public List<Item> ItemsAutoCorrect(List<Item> itemList)
-//        {
-//            //prideti kalba
-//            using (SpellEngine engine = new SpellEngine())
-//            {
-//                LanguageConfig enConfig = new LanguageConfig
-//                {
-//                    LanguageCode = "en",
-//                    HunspellAffFile = "en_us.aff",
-//                    HunspellDictFile = "en_us.dic",
-//                    HunspellKey = "",
-//                    HyphenDictFile = "hyph_en_us.dic",
-//                    MyThesDatFile = "th_en_us_new.dat"
-//                };
-//                engine.AddLanguage(enConfig);
-
-//                bool correct = engine["en"].Spell("Recommendation");
-//                Console.WriteLine("Recommendation is spelled " + (correct ? "correct" : "not correct"));
-
-//            }
-
-//            using (Hunspell hunspell = new Hunspell("en_us.aff", "en_us.dic"))
-//            {
-//                Console.WriteLine("Check if the word 'Recommendation' is spelled correct");
-//                bool correct = hunspell.Spell("Recommendation");
-//                Console.WriteLine("Recommendation is spelled " + (correct ? "correct" : "not correct"));
-
-//                Console.WriteLine("");
-//                Console.WriteLine("Make suggestions for the misspelled word 'Recommendatio'");
-//                List<string> suggestions = hunspell.Suggest("Recommendatio");
-//                Console.WriteLine("There are " + suggestions.Count.ToString() + " suggestions");
-//                foreach (string suggestion in suggestions)
-//                {
-//                    Console.WriteLine("Suggestion is: " + suggestion);
-//                }
-//            }
-//            return itemList;
-//        }
-//    }
-//}
-
-
-////https://sourceforge.net/p/nhunspell/code/ci/default/tree/NHunspellSamples/CSharpConsoleSamples/Program.cs#l68
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using GREEDY.Models;
 using NHunspell;
@@ -64,10 +11,6 @@ namespace GREEDY.ReceiptCreatings
     {
         public List<Item> ItemsAutoCorrect(List<Item> itemList)
         {
-
-            // Important: Due to the fact Hunspell will use unmanaged memory you have to serve the IDisposable pattern
-            // In this block of code this is be done by a using block. But you can also call hunspell.Dispose()
-
             using (Hunspell hunspell = new Hunspell("lt_LT.aff", "lt_LT.dic"))
             {
                 //preparing specific data
@@ -84,26 +27,24 @@ namespace GREEDY.ReceiptCreatings
                 }
 
                 //Main autoCorrect
-                bool correct;
-                List<string> suggestions;
+                string Name = String.Empty;
                 for (int i = 0; i < itemList.Count; i++)
                 {
                     foreach (string word in itemList[i].Name.Split(' '))
                     {
-                        correct = hunspell.Spell(word);
-                        Console.WriteLine(word + " is spelled " + (correct ? "correct" : "not correct"));
-
-                        if (!correct)
+                        if (!hunspell.Spell(word))
                         {
-                            Console.WriteLine("Make suggestions for the misspelled word ", word);
-                            suggestions = hunspell.Suggest(word);
-                            Console.WriteLine("There are " + suggestions.Count + " suggestions");
-                            foreach (string suggestion in suggestions)
-                            {
-                                Console.WriteLine("Suggestion is: " + suggestion);
-                            }
+                            Name += hunspell.Suggest(word).First() + " ";
+                            Console.WriteLine(Name);
+                        }
+                        else
+                        {
+                            Name += word + " ";
+                            Console.WriteLine(Name);
                         }
                     }
+                    itemList[i].Name = Name;
+                    Name = String.Empty;
                 }
             }
             return itemList;
