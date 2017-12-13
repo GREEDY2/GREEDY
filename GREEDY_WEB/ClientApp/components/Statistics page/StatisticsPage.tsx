c9c2547f7c930076487c3bb73f9f9ff944921996
MariusKurbakovas <marius.kurbakovas@gmail.com>
2017-12-13 15:59:58 +0200

    Fixes

--- GREEDY_WEB/ClientApp/components/Statistics page/StatisticsPage.tsx
+++ GREEDY_WEB/ClientApp/components/Statistics page/StatisticsPage.tsx
@@ -48,6 +48,7 @@ export class StatisticsPage extends React.Component<RouteComponentProps<{}>> {
     }
 
     getData(hours) {
+        this.setState({ showGraphs: false });
         axios.get(Constants.httpRequestBasePath + 'api/GraphData/' + hours,
             {
                 headers: {
@@ -79,13 +80,11 @@ export class StatisticsPage extends React.Component<RouteComponentProps<{}>> {
                         <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 0} onClick={() => this.getData(0)}>All time</Button>
                         <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 524160} onClick={() => this.getData(524160)}>Last year</Button>
                         <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 43200} onClick={() => this.getData(43200)}>Last month</Button>
-                        <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 10080} onClick={() => this.getData(10080)}>Last week</Button>
                     </div>
                     <div className="btn-group inline">
+                        <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 10080} onClick={() => this.getData(10080)}>Last week</Button>
                         <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 4320} onClick={() => this.getData(4320)}>Last 3 days</Button>
                         <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 1440} onClick={() => this.getData(1440)}>Last day</Button>
-                        <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 600} onClick={() => this.getData(600)}>10 Hours</Button>
-                        <Button type="button" className="btn btn-primary" disabled={this.state.hourStampForData === 120} onClick={() => this.getData(120)}>2 Hours</Button>
                     </div>
                 </div>
                 <ItemCategoryPieChart data={this.state.graphData.CategoriesData} size={this.state.size} />
